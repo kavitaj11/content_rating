27d3edc148a2ebac6a336b11d7911783467f1cb9
Kavita Jadhav <kavita.jadhav1109@gmail.com>
2025-04-03 16:04:18 -0500

    Update ContentRating.jsx
    Added counter for number of ratings.

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -1,31 +1,34 @@
-     import React, { Component } from 'react';
-     import './ContentRating.css';
+import React, { Component } from 'react';
+import './ContentRating.css';
 
-     class ContentRating extends Component {
-       constructor() {
-         super();
-         this.state = {
-             likes: 0,
-             dislikes: 0,
-           handleLike:() => {
-             this.setState((prevState) => ({
-                 likes: prevState.likes + 1
-               }));
-           },
-           handleDislike:() => {
-             this.setState((prevState) => ({
-                 dislikes: prevState.dislikes + 1
-               }));
-           }
-           };
-       }
-       render() {
-         return (
-          <>
-          <h1>Text Content Rating</h1>
-          <div className='content-rating'>
-             <p>
-             React is a popular JavaScript library for building user interfaces, especially for single-page applications (SPAs). It allows you to build components that can manage their own state and be composed into complex UIs.
+class ContentRating extends Component {
+  constructor() {
+    super();
+    this.state = {
+      likes: 0,
+      dislikes: 0,
+      totalRatings: 0,
+      handleLike: () => {
+        this.setState((prevState) => ({
+          likes: prevState.likes + 1,
+          totalRatings: prevState.totalRatings + 1
+        }));
+      },
+      handleDislike: () => {
+        this.setState((prevState) => ({
+          dislikes: prevState.dislikes + 1,
+          totalRatings: prevState.totalRatings + 1
+        }));
+      }
+    };
+  }
+  render() {
+    return (
+      <>
+        <h1>Text Content Rating</h1>
+        <div className='content-rating'>
+          <p>
+          React is a popular JavaScript library for building user interfaces, especially for single-page applications (SPAs). It allows you to build components that can manage their own state and be composed into complex UIs.
 
 A typical React app involves:
 
@@ -38,21 +41,20 @@ State & Props: State represents data that can change over time, while props are 
 React Router: For navigation and handling different views in a single-page app.
 
 Hooks: Functions like useState, useEffect, etc., that allow you to use state and other React features in functional components.
-                
-                </p>
-             <div className='rating-buttons'>
-             <button className="like-button" onClick={this.state.handleLike}>
-                 Like ({this.state.likes})
-               </button>
-               <button className="dislike-button" onClick={this.state.handleDislike}>
-                 Dislike ({this.state.dislikes})
-               </button>
-             </div>
-
+                      
+            </p>
+          <div className='rating-buttons'>
+            <button className="like-button" onClick={this.state.handleLike}>
+              Like ({this.state.likes})
+            </button>
+            <button className="dislike-button" onClick={this.state.handleDislike}>
+              Dislike ({this.state.dislikes})
+            </button>
           </div>
-          </>
-         );
-       }
-     }
-
-     export default ContentRating;
+          <p>Total Ratings: {this.state.totalRatings}</p>
+        </div>
+      </>
+    );
+  }
+}
+export default ContentRating;
